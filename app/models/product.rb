6a7eaacce4beeeec1fa5672abc94ead703b7e112
ppawlik <pawlik.peter@gmail.com>
2015-05-11 16:37:14 +0200

    Added validations and average_rating to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,17 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+  
+  validates_presence_of :title, :description, :price
+  validates_format_of :price, :with => /\A\d+(?:\.\d{0,2})?\z/
+  
+  
+  def average_rating
+    if self.reviews.size > 0
+        self.reviews.average(:rating)
+    else
+        'undefined'
+    end
+  end
+
 end
