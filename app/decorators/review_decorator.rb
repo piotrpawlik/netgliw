56ad036156b65b15037128623766a5f412b2bd5b
ppawlik <pawlik.peter@gmail.com>
2015-05-11 17:12:56 +0200

    Fullfiled author action in ReviewDecorator

--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -1,7 +1,12 @@
 class ReviewDecorator < Draper::Decorator
   delegate_all
 
-  def author  
+  def author
+    if user.firstname.blank? && user.lastname.blank?
+      'No name provided.'
+    else
+      "#{user.firstname} #{user.lastname}".strip
+    end
   end
 
 end
