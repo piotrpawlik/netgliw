0a685ff46612b73372f46e3c105191380c1cba5c
ppawlik <pawlik.peter@gmail.com>
2015-05-12 10:04:00 +0200

    Added 5 fake users and 1 admin user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'ffaker'
 
-User.create( firstname: 'Peter', lastname: 'Parker', email: 'spiderman@example.com', password: 'foobar12', password_confirmation: 'foobar12' )
+#admin
+User.create(firstname: 'Peter', 
+            lastname: 'Parker', 
+            email: 'admin@example.com', 
+            password: 'admin123', 
+            password_confirmation: 'admin123', 
+            admin: true)
 
-User.create( firstname: 'John', lastname: 'Snow', email: 'admin@example.com', password: 'admin123', password_confirmation: 'admin123', admin: true )
+5.times do
+  firstname = Faker::Name.first_name
+  lastname = Faker::Name.last_name
+  email = Faker::Internet.email(firstname + "." + lastname)
+  password = 'foobar12'
+  User.create!(firstname:   firstname,
+               lastname:    lastname,
+               email:       email,
+               password:              password,
+               password_confirmation: password)
+end
+
